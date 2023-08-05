522c702075f3567d2b59f6bed80cfb0ce1d77d15
_  gpradinett <94254671+gpradinett@users.noreply.github.com>
2023-08-04 22:42:52 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,11 +10,14 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt -y install nodejs
-#install pokemon app
+#SSH ADD
+install -m 600 -D /dev/null ~/.ssh/id_rsa
+echo "${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
+#install front Bonpland
 mkdir /home/ubuntu/tmp
 cd /home/ubuntu/tmp
-git clone https://github.com/Megh2507/Pokemon-App.git
-cd /home/ubuntu/tmp/Pokemon-App
+git clone git@github.com:Inmobiliaria-Bonpland/frontend.git
+cd /home/ubuntu/tmp/frontend
 npm install
 npm run build
 sudo rsync -av /home/ubuntu/tmp/Pokemon-App/build/ /var/www/html/
