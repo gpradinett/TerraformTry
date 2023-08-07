6388213ab654fd727007272b27cfa09ddbf1fc05
_  gpradinett <94254671+gpradinett@users.noreply.github.com>
2023-08-07 18:29:27 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,12 +10,12 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt -y install nodejs
-sudo touch /home/ubuntu/.ssh/id_rsa
+#sudo touch /home/ubuntu/.ssh/id_rsa
 #echo "${{ secrets.SSH_PRIVATE_GIT_KEY }}" > /home/ubuntu/.ssh/id_rsa
 #echo "holaaa" > /home/ubuntu/.ssh/prueba
 # echo -n "${{ secrets.SSH_PRIVATE_GIT_KEY }}" | sudo tee /home/ubuntu/.ssh/id_rsa >/dev/null
 #echo "${{ secrets.SSH_PRIVATE_GIT_KEY }}" > /home/ubuntu/.ssh/id_rsa
-sudo chmod 600 /home/ubuntu/.ssh/id_rsa
+#sudo chmod 600 /home/ubuntu/.ssh/id_rsa
 #install front Bonpland
 mkdir /home/ubuntu/tmp
 cd /home/ubuntu/tmp
