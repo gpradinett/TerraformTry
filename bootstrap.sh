c699558ef73dc6fceda3bb31be6c28076738be48
_  gpradinett <94254671+gpradinett@users.noreply.github.com>
2023-08-11 22:12:41 -0500

    v5

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,16 +10,17 @@ cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt -y install nodejs
+# Crea archivo id_rsa
 sudo chmod 400 /home/ubuntu/.ssh/id_rsa
-sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
+#sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
 # Add GitHub's public key to known_hosts
 ssh-keyscan github.com >> /home/ubuntu/.ssh/known_hosts
-sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/known_hosts
+# sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/known_hosts
 #install front Bonpland
 mkdir /home/ubuntu/tmp
-sudo chown ubuntu:ubuntu /home/ubuntu/tmp
+# sudo chown ubuntu:ubuntu /home/ubuntu/tmp
 cd /home/ubuntu/tmp
-sudo su ubuntu
+# sudo su ubuntu
 git clone git@github.com:Inmobiliaria-Bonpland/frontend.git
 cd /home/ubuntu/tmp/frontend
 git checkout dev
