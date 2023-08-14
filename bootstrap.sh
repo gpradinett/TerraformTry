ce5df0045c203dc131bf78df77878e60a88e9bc5
_  gpradinett <94254671+gpradinett@users.noreply.github.com>
2023-08-14 02:11:50 -0500

    Version 1.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,17 +11,10 @@ curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt -y install nodejs
 # Crea archivo id_rsa
-# sudo chmod 400 /home/ubuntu/.ssh/id_rsa
-# sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
 su - ubuntu -c "chmod 400 /home/ubuntu/.ssh/id_rsa"
 # Add GitHub's public key to known_hosts
-# ssh-keyscan github.com >> /home/ubuntu/.ssh/known_hosts
-# sudo chown ubuntu:ubuntu /home/ubuntu/.ssh/known_hosts
 su - ubuntu -c "ssh-keyscan github.com >> /home/ubuntu/.ssh/known_hosts"
 #install front Bonpland
-# mkdir /home/ubuntu/tmp
-# sudo chown ubuntu:ubuntu /home/ubuntu/tmp
-su - ubuntu -c "mkdir /home/ubuntu/tmp"
-# cd /home/ubuntu/tmp
-su - ubuntu -c "cd /home/ubuntu/tmp && git clone git@github.com:Inmobiliaria-Bonpland/frontend.git && cd frontend && git checkout dev && npm install && npm run build"
+su - ubuntu -c "mkdir /home/ubuntu/tmp && cd /home/ubuntu/tmp && git clone git@github.com:Inmobiliaria-Bonpland/frontend.git && cd frontend && git checkout dev && npm install && npm run build"
+# su - ubuntu -c "cd /home/ubuntu/tmp && git clone git@github.com:Inmobiliaria-Bonpland/frontend.git && cd frontend && git checkout dev && npm install && npm run build"
 sudo rsync -av /home/ubuntu/tmp/frontend/dist/ /var/www/html/
